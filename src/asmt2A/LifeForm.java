8c097206c93fd2e4fcb3ece35bce8b5ff28eaefe
Ross <rwhite83@hotmail.com>
2018-11-04 22:33:13 -0800

    first attempt at plantFeature !NOT FUNCTIONAL!

--- src/asmt2A/LifeForm.java
+++ src/asmt2A/LifeForm.java
@@ -1,8 +1,23 @@
 package asmt2A;
 
+import java.util.Random;
+
+import com.sun.javafx.geom.Vec2d;
+
 public abstract class LifeForm {
 	
 	/** abstract class with empty body methods to be defined by children */
+	
+	Vec2d UpLeft = new Vec2d(-1, 1);
+	Vec2d Up = new Vec2d(0, 1);
+	Vec2d UpRight = new Vec2d(1, 1);
+	Vec2d Left = new Vec2d(-1, 0);
+	Vec2d Right = new Vec2d(1, 0);
+	Vec2d DownLeft = new Vec2d(-1, -1);
+	Vec2d Down = new Vec2d(0, -1);
+	Vec2d DownRight = new Vec2d(-1, 1);
+
+	Vec2d[] moves = new Vec2d[] {UpLeft, Up, UpRight, Left, Right, DownLeft, Down, DownRight};
 	
 	/**
 	 * temporary method definition for testing only
--- src/asmt2A/Plant.java
+++ src/asmt2A/Plant.java
@@ -1,5 +1,9 @@
 package asmt2A;
 
+import java.util.Random;
+
+import com.sun.javafx.geom.Vec2d;
+
 public class Plant extends LifeForm implements HerbivoreEdible {
 	
 	/**
@@ -7,6 +11,14 @@ public class Plant extends LifeForm implements HerbivoreEdible {
 	 */
 	private int plantVert;
 	private int plantHori;
+	
+	private int nullNeighbors;
+	private int plantNeighbors;
+	
+	Vec2d[] fertile = new Vec2d[7];;
+	
+	/** a generic random number generator */
+	Random rand = new Random();
 	
 	/** standard constructor for Plant
 	 * @param v vertical position
@@ -17,14 +29,42 @@ public class Plant extends LifeForm implements HerbivoreEdible {
 		this.plantHori = h;
 	}
 	
-	public void spawn() {
-		//plant specific spawn commands
-		//A new plant is created in a cell if there are plants in 5 neighboring cells
-		//  Question: would it be better to go through each plant and check it's neighboring 
-		//  	cells, or iterate through the whole array and and if it finds a null cell
-		//		check how many neighboring cells are plants?
+	/**
+	 * goes through all neighboring cells, if they are within global bounds
+	 * it checks if they are herbivore edible and increments plantNeighbors count
+	 * if it is, and checks if the cell is null and increments if it is
+	 */
+	public void neighborCheck() {
+		for (int i = 0,j = 0; i < moves.length; i++) {
+			int checkTempVert = (int) moves[i].x + plantVert;
+			int checkTempHori = (int) moves[i].y + plantHori;
+			if (checkTempVert < World.worldVert && checkTempHori < World.worldHori) {
+				if (World.cell[checkTempVert][checkTempHori].life instanceof HerbivoreEdible)
+					plantNeighbors++;
+				if (World.cell[checkTempHori][checkTempHori] == null) {
+					j++;
+					nullNeighbors++;
+					Vec2d tempVect = new Vec2d(checkTempVert, checkTempHori);
+					fertile[j] = tempVect;
+				}
+			}
+		}
 	}
-	
+
+	/**
+	 * first implements neighborCheck, then picks a random number based on
+	 * the number of nullNeighbors.  it then pull 
+	 */
+	public void spawn() {
+		neighborCheck();
+		int n = rand.nextInt(nullNeighbors -1);
+		Vec2d arrayPull = fertile[n];
+		if (plantNeighbors == 4 && nullNeighbors >= 3) {
+			int spawnTempVert = (int) arrayPull.x;
+			int spawnTempHori = (int) arrayPull.y;
+			World.cell[spawnTempVert][spawnTempHori].life = new Plant(spawnTempVert, spawnTempHori);
+		}
+	}
 	
 	/**
 	 * method which responds to turn and initiates a spawn function
